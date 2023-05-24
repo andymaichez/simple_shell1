f1b0566c6d5786f6f4488480991b3a0641d36333
debano gufu <debgu2000@gmail.com>
2023-05-24 09:10:26 -0700

    betty checks 2

--- _betty0.c
+++ _betty0.c
@@ -1,38 +1,4 @@
 #include <unistd.h>
-#include "shell.h"
-
-/**
- * _strlen - Returns the length of a string.
- * @str: The string to calculate the length of.
- *
- * Return: The length of the string.
- */
-size_t _strlen(const char *str)
-{
-	size_t len = 0;
-
-	while (str[len] != '\0')
-		len++;
-
-	return (len);
-}
-
-/**
- * _puts - Writes a string to standard output.
- * @str: The string to be written.
- */
-void _puts(const char *str)
-{
-	size_t len = _strlen(str);
-	ssize_t bytes_written;
-
-	bytes_written = write(STDOUT_FILENO, str, len);
-	if (bytes_written == -1)
-	{
-		/* Handle error */
-		return;
-	}
-}
 
 /**
  * main - Entry point of the program.
@@ -42,8 +8,14 @@ void _puts(const char *str)
 int main(void)
 {
 	const char *message = "Hello, world!\n";
+	ssize_t bytes_written;
 
-	_puts(message);
+	bytes_written = write(STDOUT_FILENO, message, 14);
+	if (bytes_written == -1)
+	{
+		/* Handle error */
+		return (1);
+	}
 
 	return (0);
 }
